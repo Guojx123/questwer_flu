6b66f8814051cc4134ad5852f7b87a69fed57412
gino.gjx <gino.gjx@mobvoyage.com>
2021-02-20 09:36:58 +0800

    modify size

--- lib/theme/size.dart
+++ lib/theme/size.dart
@@ -4,5 +4,6 @@ class DefaultSize{
   static double get defaultPadding => 10.0;
   static double get basePadding => 1.0;
   static double get smallSize => 5.0;
+  static double get middleSize => 10.0;
   static double get fontSize => 16.0;
 }
